31d583d2b1e084ae2a25baab68d99ecb04ecba96
Marjorie Celestino <marjorie_celestino@hotmail.com>
2019-11-02 15:12:57 +0000

    Basic structure

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Dark Series</title>
+  </head>
+  <body>
+    
+<header>
+<!-- Menu --->
+<nav >
+</nav>
+</header>
+
+<main>
+<!-- Home --->
+	<div>
+		<div id="home-slider">
+			
+		</div>
+	</div>
+<!-- Introduction --->
+	<div class="intro-content">
+		
+	</div>
+<!-- Form --->
+  <div class="form-content">
+    
+  </div>
+<!-- Map --->
+  <div id="map">
+    
+  </div>
+</main>
+<footer>
+  
+</footer>
+</body>
+</html>
